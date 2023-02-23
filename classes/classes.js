9d7383600b1758d30389116f1d9a49bd6dc426a1
victorandresp <ponceottamendi27@gmail.com>
2023-02-23 09:17:08 -0400

    feat: static methods, getters & setters :sparkles:

--- classes/classes.js
+++ classes/classes.js
@@ -2,6 +2,7 @@ class Pokemon {
     constructor(name, gender){
         this.name = name
         this.gender = gender
+        this.type = undefined
     }
     sound(){
         console.log(`${this.name} has emitted an sound!`);
@@ -9,15 +10,32 @@ class Pokemon {
     greet(){
         console.log(`${this.name} is greeting you`);
     }
+    static info(){ // Static methods are functions that are part of a class and can be called without making an instance.
+        console.log("Pokémon are creatures of all shapes and sizes that live either in the wild or alongside humans, they are non-real characters created by nintendo in their own universe.");
+    }
+    get getType(){
+        return this.type
+    }
+    set setType(type){
+        this.type = type
+    }
 }
+
+Pokemon.info() // Call static method
 
 const bulbasaur = new Pokemon("Bulbasaur", "Male")
 const pachirisu = new Pokemon("Pachirisu", "Female")
 
 console.log(bulbasaur);
 console.log(pachirisu);
+
 pachirisu.sound()
 pachirisu.greet()
+
+// use getters and setters
+// NOTE: the use of getters and setters can be a bit strange because they should be treated as attributes(expressions) and not as methods. 
+pachirisu.setType = 'electric'
+console.log("Type:", pachirisu.getType);
 
 // Inheritance 
 class PokemonShiny extends Pokemon{

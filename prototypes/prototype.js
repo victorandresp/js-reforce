b7ccb0f69716a4484e17cb4bf667a3f42d7ae8ec
victorandresp <ponceottamendi27@gmail.com>
2023-02-22 20:11:59 -0400

    feat: prototypical inheritance :sparkles:

--- prototypes/prototype.js
+++ prototypes/prototype.js
@@ -5,7 +5,7 @@ function Pokemon(name, gender){
 
  this.sound = function(){
     console.log(`${this.name} has emitted an sound!`);
-}
+ }
 }
 // NOTE: this way for add an method to constructor function is better because saves memory in each instance of the object
 Pokemon.prototype.greet = function() {
@@ -19,3 +19,22 @@ console.log(bulbasaur);
 console.log(pachirisu);
 pachirisu.sound()
 pachirisu.greet()
+
+// Prototypical Inheritance
+function PokemonShiny(name, gender, color){
+    this.super = Pokemon
+    this.super(name, gender)
+    this.color = color
+}
+
+PokemonShiny.prototype = new Pokemon() // Transfer Inheritance
+PokemonShiny.prototype.constructor = PokemonShiny
+
+PokemonShiny.prototype.greet = function(){ // Ovewrite Inherited method 
+    console.log(`${this.name} has emitted an greeting with a shiny aura ${this.color}!`);
+}
+
+const shinyArcanine = new PokemonShiny("Shiny Arcanine", "Male", "Golden")
+console.log(shinyArcanine);
+
+shinyArcanine.greet()

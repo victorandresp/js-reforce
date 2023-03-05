4c4a43e58daa50d128d314c4e1b409cbc120ff32
victorandresp <ponceottamendi27@gmail.com>
2023-03-04 23:56:01 -0400

    feat: Promise.allSettled handling :sparkles:

--- promises/promisesMethods.js
+++ promises/promisesMethods.js
@@ -1,6 +1,6 @@
 // Promise.all
 const promiseToAllFirst = new Promise((resolve)=>{
-    setTimeout(() => resolve('Second promise all'), 500)
+    setTimeout(() => resolve('First promise all'), 500)
 })
 
 const promiseToAllSecond = new Promise((resolve)=>{
@@ -10,9 +10,16 @@ const promiseToAllFailed = new Promise((_, reject)=>{
     setTimeout(() => reject('FAILED THIRD PROMISE ALL'), 500)
 }).catch(err => new Error(err))
 
-Promise.all([promiseToAllFirst, promiseToAllSecond, promiseToAllFailed])
+Promise.all([promiseToAllFirst, promiseToAllSecond, promiseToAllFailed])  
     .then(res => {
-        console.log('THEN', res)
-        console.log('THEN', res[2].message) // How shows the error message if we are catchin
+        console.log('THEN Promise.All', res)
+        console.log(res[2].message) // How shows the error message if we are catchin
     })
     .catch(err => console.log(err))
+
+// Promise.allSettled
+
+Promise.allSettled([promiseToAllFirst, promiseToAllSecond, promiseToAllFailed])  
+    .then(res => {
+        console.log('THEN', res)
+    })
